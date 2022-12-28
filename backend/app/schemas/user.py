0651d8a1cb5bd933c319370606467ae735c6eb4e
B0arrr <aw055966@student.ath.edu.pl>
2022-12-28 23:30:45 +0100

    Modified user schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -9,14 +9,14 @@ class UserBase(BaseModel):
     last_name: str
     email: EmailStr
     password: str
-    is_active: bool
-    is_superuser: bool
-    is_employed: bool
-    date_of_employment: date
+    is_active: Optional[bool] = False
+    is_superuser: Optional[bool] = False
+    is_employed: Optional[bool] = True
+    date_of_employment: Optional[date] = date.today()
     date_of_fired: Optional[date]
-    company_id: int
-    employment_id: int
-    position_id: int
+    company_id: Optional[int]
+    employment_id: Optional[int]
+    position_id: Optional[int]
 
 
 class UserCreate(UserBase):
@@ -47,14 +47,14 @@ class UserInDBBase(UserBase):
     last_name: str
     email: EmailStr
     password: str
-    is_active: bool
-    is_superuser: bool
-    is_employed: bool
-    date_of_employment: date
+    is_active: Optional[bool]
+    is_superuser: Optional[bool]
+    is_employed: Optional[bool]
+    date_of_employment: Optional[date]
     date_of_fired: Optional[date]
-    company_id: int
-    employment_id: int
-    position_id: int
+    company_id: Optional[int]
+    employment_id: Optional[int]
+    position_id: Optional[int]
 
     class Config:
         orm_mode = True
